f5b0f87b5cd1a8c4eda3378cb4efb569b2525f54
Widya Amala <widya2609@gmail.com>
2023-03-22 18:03:40 +0700

    feat(silsilah_keluarga): create family tree page
    PIC: Widya Amala

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import SilsilahKeluarga from "./pages/silsilah_keluarga";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SilsilahKeluarga />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
