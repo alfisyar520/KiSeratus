6b4d7842b4afd91b46e39406115ea7e5375599a3
alfisyar <54832784+alfisyar520@users.noreply.github.com>
2023-03-22 13:29:31 +0700

    feat(Dashboard): add content dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,18 @@
+import {
+  Text,
+  Center,
+} from "@chakra-ui/react";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Center h="60vh">
+        <Text color="black" style={{fontSize:"50px"}}>
+          Selamat Datang di Aplikasi KiSeratus
+        </Text>
+      </Center>
+    </div>
+  );
 };
 
 export default Dashboard;
