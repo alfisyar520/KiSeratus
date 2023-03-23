97bad83eb31224d0b1121ef94931bcc85bb9f03f
Rayhan <44944844+ToxicBeastt@users.noreply.github.com>
2023-03-23 10:45:50 +0700

    feat(jadwal-4.0): add jadwal route

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import { ChakraProvider } from '@chakra-ui/react'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 import Layout from "./components/layout";
 import Dashboard from "./pages/dashboard";
 import SilsilahKeluarga from "./pages/silsilah_keluarga";
+import Jadwal from "./pages/jadwal_4.0";
 
-import './assets/styles/global.scss';
+import "./assets/styles/global.scss";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider resetCSS={true}>
       <Router>
         <Routes>
-          <Route path='/' element={<App/>}>
-            <Route path="/login" element={<Login/>} />
+          <Route path="/" element={<App />}>
+            <Route path="/login" element={<Login />} />
             <Route path="/" element={<Layout />}>
               <Route index element={<Dashboard />} />
-              <Route path="/silsilah-keluarga" element={<SilsilahKeluarga />}>
-            </Route>
+              <Route
+                path="/silsilah-keluarga"
+                element={<SilsilahKeluarga />}
+              ></Route>
+              <Route
+                path="/jadwal"
+                element={<Jadwal />}
+              ></Route>
             </Route>
           </Route>
         </Routes>
